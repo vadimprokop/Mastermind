034467c1ba443c868626945b590a88cf71a8c942
vadimprokop <pro_joy@mail.ru>
2019-01-17 10:29:56 +0100

    Update SQLQuery1.sql

--- MasterMind/Database/SQLQuery1.sql
+++ MasterMind/Database/SQLQuery1.sql
@@ -2,9 +2,8 @@ CREATE TABLE PlayersResults
 (
 	PlayerID INT IDENTITY(1,1) PRIMARY KEY,
 	PlayerNickName nvarchar(50) NOT NULL,
-	PlayerTime time NOT NULL,
 	PlayerScore int NOT NULL
 )
 go
-insert into PlayersResults values('Joy', '00:2:00', 3200)
+insert into PlayersResults values('Joy', 3200)
 select * from PlayersResults
